15140b15c0776ba572d7246c915bcc4364e6affe
JiHyunSeo46 <68982653+JiHyunSeo46@users.noreply.github.com>
2021-03-19 01:15:20 +0900

    enterprise
    기업양식

---
+++ src/main/resources/static/js/postcodify.js
@@ -0,0 +1,23 @@
+$(function () {
+  $("#postcodify").postcodify({
+    insertPostcode5: "#postcode",
+    insertAddress: "#address",
+    insertDetails: "#details",
+    insertExtraInfo: "#extra_info",
+    hideOldAddresses: false,
+    hideSummary: true,
+    useAlert: true,
+    ready: function () {
+      $("#postcodify div.postcode_search_status.empty").hide();
+    },
+    beforeSearch: function (keywords) {
+      $("#entry_box").hide();
+    },
+    afterSelect: function (selectedEntry) {
+      $("#postcodify div.postcode_search_result").remove();
+      $("#postcodify div.postcode_search_status.summary").hide();
+      $("#entry_box").show();
+      $("#entry_details").focus();
+    },
+  });
+});
