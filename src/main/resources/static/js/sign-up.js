a3cca6d06204a3182350331ed7c62ff8249138ad
JiHyunSeo46 <68982653+JiHyunSeo46@users.noreply.github.com>
2021-03-20 16:31:09 +0900

    sign-up
    4:31

---
+++ src/main/resources/static/js/sign-up.js
@@ -0,0 +1,41 @@
+let signUp = {
+  init: function () {
+    $("#submit").on("click", () => {
+      this.save();
+    });
+  },
+
+  save: function () {
+    let userData = {
+      user_id: $("#user_id").val(),
+      user_pw: $("#user_pw").val(),
+      user_name: $("#user_name").val(),
+      user_email: $("#user_email").val(),
+      user_ph: $("#user_ph").val(),
+      user_addr: $("#user_addr").val(),
+      user_dept: $("#user_dept").val(),
+      user_class: $("#user_class").val(),
+      user_job: $("#user_job").val(),
+      role: $(":input:text[name=role]:checked").val(),
+    };
+
+    console.log(userData);
+
+    $.ajax({
+      type: "POST",
+      url: "/signUp",
+      data: JSON.stringify(userData),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+    })
+      .done(function (response) {
+        alert("회원가입이 완료되었습니다");
+
+        location.href = "/index";
+      })
+      .fail(function (err) {
+        alert("회원가입을 실패하였습니다.");
+        console.log(JSON.stringify(err));
+      });
+  },
+};
