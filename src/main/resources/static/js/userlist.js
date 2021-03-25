69b1c3ca98e3d141851e9ffaf3a275d76bb69b73
yam-yam-yam-yam <77680103+yam-yam-yam-yam@users.noreply.github.com>
2021-03-26 00:42:34 +0900

    어드민 회원관리

--- src/main/resources/static/js/userlist.js
+++ src/main/resources/static/js/userlist.js
@@ -7,33 +7,75 @@ function selectAll(selectAll) {
     })
 }
 
-//구분해서 검색 ajax
-function select() {
-    var job = document.getElementsByName("job")[0].value;  //name은 중복될수 있어서[n] 추가
-    alert(job);
+// //구분해서 검색 ajax
+// function select() {
+//     var job = document.getElementsByName("job")[0].value;  //name은 중복될수 있어서[n] 추가
+//     alert(job);
 
-    var i_n = document.getElementsByName("i_n")[0].value;
-    if(i_n=="아이디"){
-        var i_n_text = document.getElementsByName("i_n_text")[0].value;
-        alert(i_n_text);
-    }
-    else if(i_n=="이름"){
-        var i_n_text = document.getElementsByName("i_n_text")[0].value;
-        alert(i_n_text);
-    }
+//     var i_n = document.getElementsByName("i_n")[0].value;
+//     if(i_n=="아이디"){
+//         var i_n_text = document.getElementsByName("i_n_text")[0].value;
+//         alert(i_n_text);
+//     }
+//     else if(i_n=="이름"){
+//         var i_n_text = document.getElementsByName("i_n_text")[0].value;
+//         alert(i_n_text);
+//     }
 
-    $.ajax({
-        url:"userSelectUrl.do",
-        type:'GET',
-        data: allData,
-        success:function(data){
-            alert("완료!");
-            window.opener.location.reload();
-            self.close();
-        },
-        error:function(jqXHR, textStatus, errorThrown){
-            alert("에러 발생~~ \n" + textStatus + " : " + errorThrown);
-            self.close();
-        }
-    });
+//     $.ajax({
+//         url:"userSelectUrl.do",
+//         type:'GET',
+//         data: allData,
+//         success:function(data){
+//             alert("완료!");
+//             window.opener.location.reload();
+//             self.close();
+//         },
+//         error:function(jqXHR, textStatus, errorThrown){
+//             alert("에러 발생~~ \n" + textStatus + " : " + errorThrown);
+//             self.close();
+//         }
+//     });
+// }
+
+
+function detail(){
+    document.getElementsByClassName("detailBox")[0].style.visibility="visible";
+    document.getElementsByClassName("detailEnter")[0].style.visibility="visible";
+
+    document.getElementsByClassName("detailBox")[0].style.opacity="1";
+    document.getElementsByClassName("detailBox")[0].style.boxShadow="0px 0px 1px 1px rgb(192, 192, 192)";
+    document.getElementsByClassName("bodyLap")[0].style.opacity="0.4";
+
 }
+
+
+let index;
+function detailClose(){
+    document.getElementsByClassName("detailBox")[0].addEventListener("click", function(e){
+        index=1; 
+    });
+    if (index==0) { // close 가 true가 아니면 아래 코드 실행
+      document.getElementsByClassName("detailBox")[0].style.visibility="hidden";
+      document.getElementsByClassName("detailEnter")[0].style.visibility="hidden";
+      document.getElementsByClassName("bodyLap")[0].style.opacity="1";
+    }
+    index=0;
+}
+
+
+
+
+//     window.onload = function(){
+//     document.getElementsByClassName("detailEnter")[0].addEventListener("click", function(e){
+//     document.getElementsByClassName("detailBox")[0].style.visibility="hidden";
+//     document.getElementsByClassName("detailEnter")[0].style.visibility="hidden";
+
+//     document.getElementsByClassName("bodyLap")[0].style.opacity="1";
+//   });
+// }
+
+
+
+
+
