4d9bc8602cd66de7035ef4907198922a8b8e8408
dodo <do8017@naver.com>
2021-03-30 14:42:51 +0900

    Empoly list , Repository, service , Controller 추가

---
+++ src/main/java/com/dung/geul/service/EmpolyService.java
@@ -0,0 +1,10 @@
+package com.dung.geul.service;
+
+
+import com.dung.geul.entity.Empoly;
+
+import java.util.List;
+
+public interface EmpolyService  {
+    List<Empoly> EmpolyList(Empoly empoly);
+}
