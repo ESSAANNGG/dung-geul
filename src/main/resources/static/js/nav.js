a0d22049a5c784e4f10fd5363b59ab6af1fa6190
JiHyunSeo46 <68982653+JiHyunSeo46@users.noreply.github.com>
2021-03-31 18:09:29 +0900

    center-navbar
    센터정보 상단메뉴 수정

--- src/main/resources/static/js/nav.js
+++ src/main/resources/static/js/nav.js
@@ -1,28 +1,27 @@
 var nav = document.getElementsByClassName("nav");
-var nav2 = document.getElementsByClassName("nav2")
-var dep2 = document.getElementsByClassName("dep2")
-var intro = document.getElementsByClassName("intro")
-var intro_center = document.getElementsByClassName("intro_center")
+var nav2 = document.getElementsByClassName("nav2");
+var dep2 = document.getElementsByClassName("dep2");
+var intro = document.getElementsByClassName("intro");
+var intro_center = document.getElementsByClassName("intro_center");
 
-function nav_on(){
-    nav[0].style.backgroundColor="white";     
-    nav2[0].style.height="163px";
-    for(var i = 0; i<dep2.length; i++){
-        dep2[i].style.height="163px"; 
-    }
+function nav_on() {
+  nav[0].style.backgroundColor = "white";
+  nav2[0].style.height = "165px";
+  for (var i = 0; i < dep2.length; i++) {
+    dep2[i].style.height = "165px";
+  }
 
-    // intro[0].style.height="200px";
-    // intro_center[0].style.fontSize="50px"
-
+  // intro[0].style.height="200px";
+  // intro_center[0].style.fontSize="50px"
 }
 
-function nav_off(){
-    /*setTimeout(function() {
+function nav_off() {
+  /*setTimeout(function() {
         nav[0].style.backgroundColor="transparent"; 
     }, 1000);             오류있음 이유모르겠*/
-    nav[0].style.backgroundColor="transparent";   /*transparent = 투명한*/
-    nav2[0].style.height="0px";
-    for(var i = 0; i<dep2.length; i++){
-        dep2[i].style.height="0px"; 
-    }
+  nav[0].style.backgroundColor = "transparent"; /*transparent = 투명한*/
+  nav2[0].style.height = "0px";
+  for (var i = 0; i < dep2.length; i++) {
+    dep2[i].style.height = "0px";
+  }
 }
