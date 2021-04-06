7ee7d9454991f729fb72cff1bebec0589b8b4b7e
cos850 <68280248+cos850@users.noreply.github.com>
2021-04-06 09:30:55 +0900

    오류 수정

--- src/test/java/com/dung/geul/repository/EmpolyRepositoryTest.java
+++ src/test/java/com/dung/geul/repository/EmpolyRepositoryTest.java
@@ -1,23 +1,23 @@
-package com.dung.geul.repository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class EmpolyRepositoryTest {
-
-    @Autowired
-    private EmpolyRepository empolyRepository;
-
-    @Test
-    public void testInsertDummies(){
-
+//package com.dung.geul.repository;
 //
-//        IntStream.rangeClosed(1,100).forEach(i -> {
-//            Empoly em = Empoly.builder().title("Sample...." + i).build();
-//            empolyRepository.save(em);
-//        });
-
-    }
-
-}
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class EmpolyRepositoryTest {
+//
+//    @Autowired
+//    private EmpolyRepository empolyRepository;
+//
+//    @Test
+//    public void testInsertDummies(){
+//
+////
+////        IntStream.rangeClosed(1,100).forEach(i -> {
+////            Empoly em = Empoly.builder().title("Sample...." + i).build();
+////            empolyRepository.save(em);
+////        });
+//
+//    }
+//
+//}
