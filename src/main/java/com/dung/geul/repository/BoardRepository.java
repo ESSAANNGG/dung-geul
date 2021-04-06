5b22d9cf66af64ef6e0855c970168778325d7c95
ksh3760 <63641306+ksh3760@users.noreply.github.com>
2021-04-06 20:03:25 +0900

    update notice_board_form without front design
    업데이트 공지사항 게시판 글작성 페이지

--- src/main/java/com/dung/geul/repository/BoardRepository.java
+++ src/main/java/com/dung/geul/repository/BoardRepository.java
@@ -1,18 +1,19 @@
-package com.bbs.myBbs.repository;
+package com.dung.geul.repository;
 
-import com.bbs.myBbs.model.Board;
+import com.dung.geul.entity.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-
+// 보드 레포지토리 인터페이스는 컨트롤러의 model을 사용할 수 있도록 한다.
 // 보드레포지토리 인터페이스는 jpa레포지토리를 상속받음<모델클래스, PK>
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    List<Board> findByTitle(String title);
-    List<Board> findByTitleOrContent(String title, String content);
+/*    List<Board> findByTitle(String board_title);
+    List<Board> findByTitleOrContent(String board_title, String board_content);
 
-    Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
-
+    Page<Board> findByTitleContainingOrContentContaining(String board_title, String board_content, Pageable pageable);
+*/
 }
