de1e8d29381286539e340c943f9edb41e3f8cdfd
ksh3760 <oksh3760@naver.com>
2021-04-08 03:40:40 +0900

    update job_info
    직업 정보 페이지 수정,
    직업 정보 페이지 매핑,
    공지사항 게시판 수정,
    메인 페이지 : 직업 정보 페이지 이동 링크 수정

--- src/main/java/com/dung/geul/controller/CareerPlanningController.java
+++ src/main/java/com/dung/geul/controller/CareerPlanningController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class CareerPlanningController {
 
-    @GetMapping("/simli_test")
+    @GetMapping("/simli_test")  // 심리 검사
     public String simli_test() { return "career-planning/simli_test";}
+
+    @GetMapping("job_info")  // 직업정보 조회
+    public String job_info() { return "career-planning/job_info";}
+
+
 }
