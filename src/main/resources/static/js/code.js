6eace8f0fe850df76a5d87cd289933d19f715c42
Ron <ccc13323@gmail.com>
2021-04-12 13:20:20 +0900

    회원가입 추가
    1. 교직원 회원가입 추가
    2. 주소정보에 지번주소 삭제
    3. 자바스크립트 오류 수정

--- src/main/resources/static/js/code.js
+++ src/main/resources/static/js/code.js
@@ -19,7 +19,7 @@ function carrerPostcode() {
 
       document.getElementById("user_postcode").value = data.zonecode;
       document.getElementById("user_addr").value = roadAddr;
-      document.getElementById("user_jibunAddr").value = data.jibunAddress;
+      // document.getElementById("user_jibunAddr").value = data.jibunAddress;
 
       if (roadAddr !== "") {
         document.getElementById("extra_info").value = extraRoadAddr;
@@ -32,11 +32,13 @@ function carrerPostcode() {
         var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
         guideTextBox.innerHTML = "(예상 도로명 주소 : " + expRoadAddr + ")";
         guideTextBox.style.display = "block";
-      } else if (data.autoJibunAddress) {
-        var expJibunAddr = data.autoJibunAddress;
-        guideTextBox.innerHTML = "(예상 지번 주소 : " + expJibunAddr + ")";
-        guideTextBox.style.display = "block";
-      } else {
+      }
+      // else if (data.autoJibunAddress) {
+      //   var expJibunAddr = data.autoJibunAddress;
+      //   guideTextBox.innerHTML = "(예상 지번 주소 : " + expJibunAddr + ")";
+      //   guideTextBox.style.display = "block";
+      // }
+      else {
         guideTextBox.innerHTML = "";
         guideTextBox.style.display = "none";
       }
