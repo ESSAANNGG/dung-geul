e0a3166d7577c473006628e397c96d0197a8daa5
Ron <ccc13323@gmail.com>
2021-04-14 16:46:47 +0900

    비밀번호 확인 기능 추가
    11

---
+++ src/main/resources/static/js/check-pw.js
@@ -0,0 +1,14 @@
+function check_pw() {
+  //비밀번호 확인
+  var p = document.getElementById("user_pw").value;
+  var p_cf = document.getElementById("checkPw").value;
+
+  if (p != p_cf) {
+    document.getElementById("checkPwResult").style = "color: red;";
+    document.getElementById("checkPwResult").innerHTML =
+      "비밀번호가 다릅니다. 다시 확인해 주세요.";
+  } else if ((p = p_cf)) {
+    document.getElementById("checkPwResult").style = "color: black;";
+    document.getElementById("checkPwResult").innerHTML = "비밀번호가 같습니다.";
+  }
+}
