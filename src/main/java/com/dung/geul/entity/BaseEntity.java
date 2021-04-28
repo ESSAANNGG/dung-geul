52dee23972d7dd0471ec9111a814cf66ba7c1c19
ksh3760 <oksh3760@naver.com>
2021-04-28 15:58:00 +0900

    update notice-board
    1. 검색 처리 적용

--- src/main/java/com/dung/geul/entity/BaseEntity.java
+++ src/main/java/com/dung/geul/entity/BaseEntity.java
@@ -10,7 +10,9 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@MappedSuperclass
+// entity 객체의 등록 시간과 최종 수정 시간을 담당하는 추상 클래스
+
+@MappedSuperclass   // 해당 어노테이션이 적용된 클래스는 테이블로 생성되지 않는다.
 @EntityListeners(value={AuditingEntityListener.class})
 @Getter
 public class BaseEntity {
