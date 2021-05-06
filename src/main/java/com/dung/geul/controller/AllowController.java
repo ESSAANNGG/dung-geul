a9de42afb8402900a9ca2d53e7a0cbf66c08e26f
cos850 <68280248+cos850@users.noreply.github.com>
2021-05-06 15:36:11 +0900

    이력서 수정

--- src/main/java/com/dung/geul/controller/AllowController.java
+++ src/main/java/com/dung/geul/controller/AllowController.java
@@ -6,14 +6,17 @@ import com.dung.geul.dto.PageRequestDTO;
 import com.dung.geul.dto.PageResultDTO;
 import com.dung.geul.entity.Enterprise;
 import com.dung.geul.entity.Member;
+import com.dung.geul.security.dto.AuthMemberDTO;
 import com.dung.geul.service.MemberServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -25,9 +28,16 @@ public class AllowController {
     @Autowired
     private MemberServiceImpl memberService;
 
-    // 전체 회원 인증 전 리스트
+    // 관리자만
+    @GetMapping("/")
+    public String admin(@AuthenticationPrincipal AuthMemberDTO member){
+        if(member.getUser_id().equals("admin")) return "/admin/admin";
+        else return "/";
+    }
+
+    // 전체 회원 인증 리스트 페이지
     @GetMapping("/admin")
-    public void getList(@RequestParam("type") String type ,@RequestParam("page1") int page1, @RequestParam("page2") int page2, Model model){
+    public void getList(@RequestParam("type") String type ,@RequestParam("page1") Integer page1, @RequestParam("page2") Integer page2, Model model){
         //파라미터로 page, size 를 전달하면 자동으로 pageRequestDTO 객체로 수집된다
 
         // type : USER / ENTERPRISE / STUDENT / STAFF / COUNSELOR / UNIV
@@ -40,14 +50,27 @@ public class AllowController {
         if(type==null || type.equals("")){
             type = "USER";
         }
+        if(page1 == null) {
+            page1 = 1;
+        }
+        if(page2 == null){
+            page2 = 1;
+        }
 
-        // allow = 0 : 미인증 목록
-        // allow = 1 : 인증 목록
-        List<AllowEtpDTO> notAllowList = memberService.getUserList(page1, type, 0).getDtoList();
-        List<AllowEtpDTO> AllowList = memberService.getUserList(page2, type, 1).getDtoList();
+        // allow = 0, page1 : 미인증 목록
+        // allow = 1, page2 : 인증 목록
+        PageResultDTO<AllowEtpDTO, Object[]> notAllowPageResult = memberService.getUserList(page1, type, 0);
+        PageResultDTO<AllowEtpDTO, Object[]> allowPageResult = memberService.getUserList(page2, type, 1);
+
+        List<AllowEtpDTO> notAllowList = notAllowPageResult.getDtoList();
+        List<AllowEtpDTO> AllowList = allowPageResult.getDtoList();
 
         model.addAttribute("notAllowList", notAllowList);
         model.addAttribute("allowList", AllowList);
+
+        model.addAttribute("allowPageList", allowPageResult.getPageList());
+        model.addAttribute("notAllowPageList", notAllowPageResult.getPageList());
+
 
         System.out.println(notAllowList.toString());
         System.out.println(AllowList.toString());
