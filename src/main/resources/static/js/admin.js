a7789d53873512ab2df56d6218b392bb9be4f83a
yam-yam-yam-yam <77680103+yam-yam-yam-yam@users.noreply.github.com>
2021-05-09 02:31:19 +0900

    admin 살짝정리
    햄버거는 핫크리스피햄버거

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -87,102 +87,9 @@ $('.guide_select').change(function(){
 function submit_param(){  //메뉴클릭,가이드메뉴 선택시
     location.href="/admin/admin?type=" + parameter + "&page1=1&page2=1";
 }
-function submit_data(){
+function submit_data(){   //
     location.href="/admin/admin?type=" + parameter + "&page1=1&page2=1";
 }
-
-//파라미터 보내기
-///////////////////////////////////////////////
-
-//main2_1&&main2_2 date
-//search_date_num가 1==오늘 2==이번주 3==이번달 4==전체
-let date_range=document.getElementsByClassName('user_search_date');
-function search_date(main_num,date_select){
-    let now=new Date();
-    let week=new Date();
-    let month=new Date();
-    let enter=new Date(1);  //파라미터를 한개만 전송하면 1970년도로 자동설정
-    let dateVar;
-    week.setDate(now.getDate()-7);
-    month.setMonth(now.getMonth()-1);
-    if(main_num=="main2_user") {
-        switch (date_select) {
-            case '오늘': dateVar=now;
-                break;
-            case '이번주': dateVar=week;
-                break;
-            case '이번달': dateVar=month;
-                break;
-            case '전체': dateVar=enter;
-                break;
-        }
-        date_range[0].value = dateVar.toISOString().substring(0, 10);
-        date_range[1].value = now.toISOString().substring(0, 10);
-    }
-    else if(main_num=="main2_corp"){
-        switch (date_select) {
-            case '오늘': dateVar=now;
-                break;
-            case '이번주': dateVar=week;
-                break;
-            case '이번달': dateVar=month;
-                break;
-            case '전체': dateVar=enter;
-                break;
-        }
-        date_range[2].value = dateVar.toISOString().substring(0, 10);
-        date_range[3].value = now.toISOString().substring(0, 10);
-    }
-
-}
-
-
-
-// 회원 상세정보
-let non_detail=0;       //.user_list_body안에 있는 체크박스나 select(기업형태)를 클릭했을시 상세정보를 띄우지 않게하기위한 참조변수
-let detail_state=0;     //상세정보페이지가 켜져있는지 꺼져있는지 확인하기 위한 참조변수;
-let detail_per;          //어떤 권한의 사용자인지 확인하는 변수 0=학생 1=교직원 2=상담사 3=기업
-$('.user_list_body :checkbox, .shape').click(function(){
-    non_detail=1;
-})
-//체크박스나 select를 클릭하였다면 상세정보를 띄우지않는다.
-//non_detail=0이면 상세정보를 띄워줌
-function detail(users) {
-    if (non_detail == 1) {
-        non_detail = 0;
-        return;
-    } else if (non_detail == 0) {
-        users_roll=$(users).children('span.role').text();                 //role을 읽어옴
-        setTimeout("detail_on(users_roll)", 100);          //settimeout을 하지않으면 detail_state=1이되어 바로 상세정보를 닫아버림
-    }
-}
-
-function detail_on(users_roll){
-    switch (users_roll) {
-        case '학생': detail_per="detail_student";
-            break;
-        case '교직원': detail_per="detail_staff";
-            break;
-        case '상담사': detail_per="detail_counselor";
-            break;
-        case '기업': case '' : detail_per="detail_enterprise";
-            break;
-    }
-    $('#'+detail_per).css({"visibility":"visible","opacity":"1"});
-    $('#wrap,#admin_header').css("opacity","0.4");
-    detail_state=1;
-}
-
-//상세정보를 닫음
-$('#shadow_box').click(function(e){
-    if(detail_state==1) {
-        $('.detailBox').css({"visibility": "hidden", "opacity": "0"});
-        $('#wrap,#admin_header').css("opacity", "1");
-        detail_state = 0;
-    }
-})
-
-
 
 
 //게시판 전체 체크하기
@@ -198,10 +105,7 @@ function checkAll(checkI) {
     }
 }
 
-
-
-
-//회원가입승인 허가
+//회원가입승인 허가,거절 회원삭제
 let p; //승인인지 거절인지 html으로부터 받아옴
 let perList=[]; //userid값을 담아넣는 배열
 let userid //userid값을 하나하나 담음
@@ -213,8 +117,8 @@ function permission_ajax(p){
     for(j=0; j<perLength; j++){
         let perRemove=($("input[name='2_2_check']").index($('input[name="2_2_check"]:checked')));     //회원가입승인 전체 체크중 체크된것들의 인덱스를 가져옴
 
-        userid=$('.user_list:eq(1) .user_list_body:eq(' + perRemove + ') .username').text();                    //아이디값을 읽어옴
-        perList.push(userid);                                                                                    //전달할 배열에 값 삽입
+        userid=$('.user_list:eq(1) .user_list_body:eq(' + perRemove + ') .username').text();          //아이디값을 읽어옴
+        perList.push(userid);                                                                         //전달할 배열에 값 삽입
         ($('input[name="2_2_check"]').eq(perRemove)).prop("checked",false);                           //해당하는 인덱스의 체크 해제
         $('input[name="2_2_checkH"]').prop("checked",false);                                          //헤드checkBox 체크 해제
     }
@@ -235,11 +139,10 @@ function permission_ajax(p){
         dataType: "json",
         data: JSON.stringify(perList),
     })
-
     submit_data();
 }
 
-//기업회원가입 승인
+//기업가입승인 허가,거절 기업삭제
 let userShape;
 let E_perList=[];
 let E_perLength;
@@ -281,7 +184,5 @@ function E_permission_ajax(p){
         type:"POST",
         data: E_perList
     })
-
     submit_data();
-
 }
