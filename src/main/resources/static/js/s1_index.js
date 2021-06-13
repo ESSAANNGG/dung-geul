dc7bcfdcbb3ae91f3f187dac115cd3307285206f
JiHyunSeo46 <68982653+JiHyunSeo46@users.noreply.github.com>
2021-06-13 22:03:21 +0900

    cv 자격증 추가버튼, 모달추가

---
+++ src/main/resources/static/js/s1_index.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const bodyBlackout = document.querySelector('.body-blackout');
+  const popupModal = document.querySelector('.popup-modal');
+
+  document.querySelector('.popup-trigger').addEventListener('click', function () {
+    popupModal.classList.add('is--visible');
+    bodyBlackout.classList.add('is-blacked-out');
+  });
+
+  popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
+    popupModal.classList.remove('is--visible');
+    bodyBlackout.classList.remove('is-blacked-out');
+  });
+
+  bodyBlackout.addEventListener('click', () => {
+    popupModal.classList.remove('is--visible');
+    bodyBlackout.classList.remove('is-blacked-out');
+  });
+});
+
+let bodyBlackout = document.querySelectorAll('.body-blackout');
+let popupModal = document.querySelectorAll('.popup-modal');
+let popupTrigger = document.querySelectorAll('.popup-trigger');
+let popupModalClose = document.querySelectorAll('.popup-modal__close');
+
+document.querySelector('#add_lic').addEventListener('click', () => {
+  setTimeout(function () {
+    bodyBlackout = document.querySelectorAll('.body-blackout');
+    popupModal = document.querySelectorAll('.popup-modal');
+    popupTrigger = document.querySelectorAll('.popup-trigger');
+    popupModalClose = document.querySelectorAll('.popup-modal__close');
+
+    for (let i = 0; i < popupTrigger.length; i++) {
+      popupTrigger[i].addEventListener('click', () => {
+        popupModal[i].classList.add('is--visible');
+        bodyBlackout[i].classList.add('is-blacked-out');
+      });
+
+      popupModalClose[i].addEventListener('click', () => {
+        popupModal[i].classList.remove('is--visible');
+        bodyBlackout[i].classList.remove('is-blacked-out');
+      });
+
+      bodyBlackout[i].addEventListener('click', () => {
+        popupModal[i].classList.remove('is--visible');
+        bodyBlackout[i].classList.remove('is-blacked-out');
+      });
+    }
+  }, 100);
+});
