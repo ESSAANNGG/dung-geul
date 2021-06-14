bd138ea0e22a073c0196186ce605b6fd53f898ae
ksh3760 <oksh3760@naver.com>
2021-06-15 02:09:47 +0900

    update job info search
    update 키워드 검색

--- src/main/java/com/dung/geul/controller/restcontroller/JobInformationRestController.java
+++ src/main/java/com/dung/geul/controller/restcontroller/JobInformationRestController.java
@@ -8,7 +8,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 @RestController
-public class JobDictionaryRestController {
+public class JobInformationRestController {
 
     private  String authKey = "WNKO266ZTVI72UBY9LVWV2VR1HK";
 
@@ -111,6 +111,48 @@ public class JobDictionaryRestController {
 
         return result + "";
     }
+
+    @PostMapping("/jobsearch_k")    // 직업 정보 조회(키워드 검색)
+    public String jobsearch_k(String searchCode) {
+
+        System.out.println("================================================================");
+        System.out.println("클라이언트 측으로 부터 입력 받은 키워드 : " + searchCode);
+        System.out.println("================================================================");
+
+        StringBuffer result = new StringBuffer();
+        try {
+            String apiUrl = "http://openapi.work.go.kr/opi/opi/opia/jobSrch.do?authKey=" +
+                    authKey +
+                    "&returnType=XML&target=JOBCD&keyword=" +
+                    searchCode;
+
+            URL url = new URL(apiUrl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+
+            String returnLine;
+            // xml 태그는 모든 html 태그들을 일반 문자로 인식해서 모든 문자와 모든 태그들을 일반 문자처럼 출력해주는 동시에
+            // 태그로서의 기능은 상실해 작동하지 않게 된다.
+            result.append("");
+
+            while ((returnLine = bufferedReader.readLine()) != null) {
+                result.append(returnLine + "\n");
+            }
+
+            urlConnection.disconnect();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("result : " + result);   // 테스트용
+        System.out.println("================================================================");
+
+        return result + "";
+    }
+
 
 
 }
