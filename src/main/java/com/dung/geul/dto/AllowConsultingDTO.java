eb6202bdc07add4ba930df5ef4447513622dbee9
LSJ97 <77714886+LSJ97@users.noreply.github.com>
2021-06-17 09:10:55 +0900

    DTO추가

---
+++ src/main/java/com/dung/geul/dto/AllowConsultingDTO.java
@@ -0,0 +1,11 @@
+package com.dung.geul.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AllowConsultingDTO {
+
+    private String consult_num;
+}
