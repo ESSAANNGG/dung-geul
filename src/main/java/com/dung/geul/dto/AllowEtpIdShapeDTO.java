30a918a228d3cc01e6717cb0018dc9ed0e5a2f93
cos850 <68280248+cos850@users.noreply.github.com>
2021-06-25 10:01:16 +0900

    기업회원 인증 안되는거 고침

--- src/main/java/com/dung/geul/dto/AllowEtpIdShapeDTO.java
+++ src/main/java/com/dung/geul/dto/AllowEtpIdShapeDTO.java
@@ -2,9 +2,11 @@ package com.dung.geul.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AllowEtpIdShapeDTO {
 
     private String user_id;
