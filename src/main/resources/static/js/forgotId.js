27b06901dd96881bc78650937ac5655b83b3c222
yam-yam-yam-yam <77680103+yam-yam-yam-yam@users.noreply.github.com>
2021-07-04 18:50:14 +0900

    비밀번호찾기 jquery 값 지정

--- src/main/resources/static/js/forgotId.js
+++ src/main/resources/static/js/forgotId.js
@@ -3,41 +3,79 @@ let findID = {
         console.log("로드 성공");
         $("#findIdBtn").on("click", () => {
             console.log("findId() 실행");
-            this.findId();
+            this.findId(1);
+        });
+        $("#findphoneBtn").on("click", () => {
+            console.log("findId() 실행");
+            this.findId(2);
         });
     },
 
-    findId: function () {
-        let userData = {
-            user_name : $('#find_id_name').val(),
-            user_email : $('#find_id_email').val()
-        };
+    findId: function (num) {
+        if (num == 1) {
+            let userData = {
+                user_name: $('#find_id_name').val(),
+                user_email: $('#find_id_email').val()
+            };
 
-        console.log(JSON.stringify(userData));
+            console.log(JSON.stringify(userData));
 
-        $.ajax({
-            type: "post",
-            url: "/forgot/id",
-            data: JSON.stringify(userData),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success : function (result) {
+            $.ajax({
+                type: "post",
+                url: "/forgot/id",
+                data: JSON.stringify(userData),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (result) {
 
-                console.log('result : ' + result.user_id);
-                alert(result);
-                if(result =='0'){
-                    alert("회원정보 찾기에 실패했습니다.\n이름과 이메일을 다시 확인해주세요.");
-                } else{
-                    alert("당신의 아이디는" + result.user_id);
-                    location.href = "/login";
+                    console.log('result : ' + result.user_id);
+                    alert(result);
+                    if (result == '0') {
+                        alert("회원정보 찾기에 실패했습니다.\n이름과 이메일을 다시 확인해주세요.");
+                    } else {
+                        alert("당신의 아이디는" + result.user_id);
+                        location.href = "/login";
+                    }
+                },
+                error: function (err) {
+                    console.log(err);
+                    alert(err);
+                    alert("오류 발생.\n이름과 이메일을 다시 확인해주세요.");
                 }
-            },
-            error : function (err) {
-                console.log(err);
-                alert(err);
-                alert("오류 발생.\n이름과 이메일을 다시 확인해주세요.");
-            }
-        })
+            })
+        }
+        else if (num == 2) {
+            let userData = {
+                user_name: $('#find_phone_id').val(),
+                user_phone: $('#find_phone').val()
+            };
+
+            console.log(JSON.stringify(userData));
+
+            // $.ajax({
+            //     type: "post",
+            //     url: "/forgot/id",
+            //     data: JSON.stringify(userData),
+            //     contentType: "application/json; charset=utf-8",
+            //     dataType: "json",
+            //     success: function (result) {
+            //
+            //         console.log('result : ' + result.user_id);
+            //         alert(result);
+            //         if (result == '0') {
+            //             alert("회원정보 찾기에 실패했습니다.\n이름과 이메일을 다시 확인해주세요.");
+            //         } else {
+            //             alert("당신의 아이디는" + result.user_id);
+            //             location.href = "/login";
+            //         }
+            //     },
+            //     error: function (err) {
+            //         console.log(err);
+            //         alert(err);
+            //         alert("오류 발생.\n이름과 이메일을 다시 확인해주세요.");
+            //     }
+            // })
+        }
     }
 }
 
--- src/main/resources/static/js/forgotPw.js
+++ src/main/resources/static/js/forgotPw.js
@@ -3,39 +3,77 @@ let find = {
         console.log("로드 성공");
         $("#findPwBtn").on("click", () => {
             console.log("findPw() 실행");
-            this.findPw();
+            this.findPw(1);
+        });
+        $("#findPwBtn_phone").on("click", () => {
+            console.log("findPw() 실행");
+            this.findPw(2);
         });
     },
 
-    findPw: function () {
-        let userData = {
-            user_email : $('#find_pw_email').val(),
-            user_id : $('#find_pw_id').val()
-        };
+    findPw: function (num) {
+        if (num == 1) {
+            let userData = {
+                user_email: $('#find_pw_email').val(),
+                user_id: $('#find_pw_id').val()
+            };
 
-        console.log(JSON.stringify(userData));
+            console.log(JSON.stringify(userData));
 
-        $.ajax({
-            type: "post",
-            url: "/forgot/pw",
-            data: JSON.stringify(userData),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success : function (result) {
+            $.ajax({
+                type: "post",
+                url: "/forgot/pw",
+                data: JSON.stringify(userData),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (result) {
 
-                console.log(result);
+                    console.log(result);
 
-                if(result == 1){
-                    alert("임시 비밀번호 발급이 완료되었습니다.\n로그인 페이지로 이동합니다.");
+                    if (result == 1) {
+                        alert("임시 비밀번호 발급이 완료되었습니다.\n로그인 페이지로 이동합니다.");
 
-                    location.href = "/mypage/member/read";
-                } else{
-                    alert("회원정보 찾기에 실패했습니다.\n아이디와 이메일을 다시 확인해주세요.");
+                        location.href = "/mypage/member/read";
+                    } else {
+                        alert("회원정보 찾기에 실패했습니다.\n아이디와 이메일을 다시 확인해주세요.");
+                    }
+
                 }
+            })
+        }
+        
+        //휴대폰
+        else if(num==2){
+            let userData = {
+                user_phone: $('#find_pw_phone').val(),
+                user_id: $('#find_pw_ido').val()
+            };
 
-            }
-        })
+            console.log(JSON.stringify(userData));
+
+            // $.ajax({
+            //     type: "post",
+            //     url: "/forgot/pw",
+            //     data: JSON.stringify(userData),
+            //     contentType: "application/json; charset=utf-8",
+            //     dataType: "json",
+            //     success: function (result) {
+            //
+            //         console.log(result);
+            //
+            //         if (result == 1) {
+            //             alert("임시 비밀번호 발급이 완료되었습니다.\n로그인 페이지로 이동합니다.");
+            //
+            //             location.href = "/mypage/member/read";
+            //         } else {
+            //             alert("회원정보 찾기에 실패했습니다.\n아이디와 이메일을 다시 확인해주세요.");
+            //         }
+            //
+            //     }
+            // })
+        }
     }
+    
 }
 
 find.init();
