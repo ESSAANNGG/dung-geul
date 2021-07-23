ce24286a19f4f8647ded31334fb2b8399159ebea
yamam <diacl4927@naver.com>
2021-07-23 14:54:48 +0900

    메인화면 값 가져옴

--- src/main/java/com/dung/geul/controller/WebController.java
+++ src/main/java/com/dung/geul/controller/WebController.java
@@ -1,15 +1,35 @@
 package com.dung.geul.controller;
 
+import com.dung.geul.dto.PageRequestDTO;
+import com.dung.geul.service.EmployService;
+import com.dung.geul.service.notice_boardService;
+import com.dung.geul.service.notice_boardServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
 
 @Controller
+@RequiredArgsConstructor    // 페이지 목록 처리
+@Log4j2
 public class WebController {
 
+    @Autowired
+    notice_boardServiceImpl boardservice;
+    private final notice_boardService service;
+
+    @Autowired
+    private EmployService Eservice;
+
     @GetMapping({"/", ""})
-    public String index(Principal member){
+    public String index(Principal member, PageRequestDTO pageRequestDTO, Model model){
+
+        model.addAttribute("result", service.getList(pageRequestDTO));
+        model.addAttribute("Eresult", Eservice.getList(pageRequestDTO));
 
         try {
             if (member.getName().equals("admin")) {
